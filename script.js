e76f8a20163c5ac5c492fc9473528726f976b2a1
SID-2007-tech <sp8k2007@gmail.com>
2026-03-18 14:27:48 +0530

    borders removed and bugs fixed(1 bug remains- boxes are not squares

--- script.js
+++ script.js
@@ -15,27 +15,41 @@ function createGrid(gridSize) {
   row.classList.add("row");
   const column = document.createElement("div");
   column.classList.add("column");
-  
+
   //column.textContent="hello";
-  for (let i = 1; i <= gridSize; i++) row.appendChild(column.cloneNode("true"));
-  for (let i = 1; i <= gridSize; i++) grid.appendChild(row.cloneNode("true"));
+  for (let i = 1; i <= gridSize; i++) row.appendChild(column.cloneNode(true));
+  for (let i = 1; i <= gridSize; i++) grid.appendChild(row.cloneNode(true));
   document.body.appendChild(grid);
   let allCol = document.querySelectorAll(".column");
-  allCol.forEach((x)=>resizer(x,h));
+  allCol.forEach((x) => resizer(x, h));
 }
-
+function resetPage() {
+  console.log("resetPage function invoked");
+  
+  document.querySelector("body").removeChild(document.querySelector(".grid"))
+  createGrid(askGridSize());
+  let allCol = document.querySelectorAll(".column");
+  allCol.forEach((x) => {
+    x.addEventListener("mouseenter", (e) => changeColor(e.target));
+  });
+}
 function changeColor(x) {
   x.style.backgroundColor = "red";
 }
-function resizer(x,h) {
+ function resizer(x, h) {
+  console.log("resizer function invoked")
   x.style.height = h + "px";
   x.style.width = h + "px";
 }
+const resetBtn = document.querySelector("#resetBtn");
+resetBtn.addEventListener("click", ()=>resetPage());
 
 createGrid(askGridSize());
-let allCol=document.querySelectorAll(".column");
-allCol.forEach((x)=>{
-    x.addEventListener("mouseenter", (e) => changeColor(e.target));
-})
+let allCol = document.querySelectorAll(".column");
+allCol.forEach((x) => {
+  x.addEventListener("mouseenter", (e) => changeColor(e.target));
+});
+
+
 
 
